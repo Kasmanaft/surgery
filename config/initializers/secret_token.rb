ba338eb640928f90b438d3baa0e08477f01d3d86
Leonid Sobolievskyi <leonid@sobolievskyi.name>
2013-02-17 16:35:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surgery::Application.config.secret_token = 'c5ecb67554628bc583fda3ed486d552061e9ff811d2b7d857563f06d45b440bee34c0b800679f7eae4008db3819b9b06ffd05c6abdc33398ebc177954f501343'
